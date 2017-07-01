f82daafc47a5cd7575249a73fcf56b19f0d3c845
Sail338 <srihari.shankar1@gmail.com>
2017-07-01 10:35:24 -0400

    got basic syntax hlighting

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 const electron = require('electron');
-const Quill = require('quill')
-
-let basicEditor = new Quill('#editor', {
-  theme: 'snow',
+var Quill  = require('Quill');
+hljs.configure({   // optionally configure hljs
+  languages: ['javascript', 'ruby', 'python']
+});
+console.log(hljs);
+var basiceditor  = new Quill('#editor', {
   modules: {
-	syntax:true
-	}
+    syntax: true,              // Include syntax module
+    toolbar: [['code-block']]  // Include button in toolbar
+  },
+  theme: 'snow'
 });
--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
-
+const hljs = require('highlight.js');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -14,7 +14,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
+  mainWindow.openDevTools();
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
