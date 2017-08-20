0c107c745d1a3cacc9ae366b3d62d976194b1941
Sail338 <srihari.shankar1@gmail.com>
2017-08-20 11:41:33 -0400

    update typesctipt

--- app/firstUse.ts
+++ app/firstUse.ts
@@ -18,6 +18,31 @@ function checkPath(p:string){
  * navigates to the sign in page if its first use
  */
 //open the first use page
-function openFirstUseServer(){
-     
+function openFirstUse(){
+  let mainWindow;
+  createWindow();
+
+}
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.openDevTools();
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'firstUse.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
 }
