0f0f143564e05c7938c9e128005510e6e8d961b5
Sail338 <srihari.shankar1@gmail.com>
2018-05-15 22:30:10 -0400

    its more typescripty

--- app/firstUse.ts
+++ app/firstUse.ts
@@ -2,9 +2,18 @@ import * as fs from 'fs'
 import * as electron from 'electron'
 import * as http from 'http'
 import * as Store from 'electron-store'
+import * as $ from 'jquery'
+import {remote} from "electron"
+var dialog = remote.dialog
 function setCurrLanuage(lang:String){
     // set curr lanhuage equal to whatver we set
-     const store = new Store();
+     let store = new Store();
      store.set('lang',lang)
 
-}
+
+    }
+
+ $('#java').on('click', function () {
+       setCurrLanuage("java")
+       console.log(dialog.showSaveDialog({}))
+    })
